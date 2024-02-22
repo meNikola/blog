757aa9c937288fcca7f83e801f02ce604be8cb35
Nikola Kovacevic <kovacvb@hotmail.com>
2024-02-22 22:09:23 +0100

    Initial commit. Add a few routes.

---
+++ app/Models/Post.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    // If we want to join all related data always.
+    /*
+    protected $with = ['category', 'author'];
+    */
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        /*
+        User::truncate();
+        Category::truncate();
+        Post::truncate();
+        */
+
+        $user = User::factory()->create([
+            'name' => 'Test User',
+        ]);
+
+        Post::factory(5)->create([
+            'user_id' => $user->id
+        ]);
+    }
+}
